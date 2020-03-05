09a2343a5e7f4a06bbe748752992ed6fc823af6d
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-05 14:04:12 -0600

    Added validation to the form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 import * as ui from './interfaz.js'
 
 console.log(ui)
+
+ui.formToSearch.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const artist=document.querySelector('#artista').value
+    const song=document.querySelector('#cancion').value
+    
+    if (artist==='' || song==='') {
+        //Error by incomplete data
+        ui.mensajes.innerHTML='Error... all fields are required'
+        ui.mensajes.classList.add('error')
+        setTimeout(() => {
+            ui.mensajes.innerHTML=''
+            ui.mensajes.classList.remove('error')
+        }, 2000);
+    } else {
+        //Form complete
+    }
+})
